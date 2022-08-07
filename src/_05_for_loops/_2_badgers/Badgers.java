cbc9ceb60bb34eb023219a8390beaee00b86de0a
root <root@5bdf85579759>
2022-08-07 19:57:37 +0000

    I finished module 1

--- src/_05_for_loops/_2_badgers/Badgers.java
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -5,6 +5,29 @@ public class Badgers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int badgers = 12;
+		int snake = 1;
+		int mushroom = 2;
+		int Badgers = 0;
+		int Snake = 0;
+		int Mushroom = 0;
+		for (int i = 0; i < 2; i++) {
+			for (Badgers = 0; Badgers < badgers; Badgers += 1) {
+				System.out.print("badgers, ");
+			}
+			System.out.println();
+			for (Mushroom = 0; Mushroom < mushroom; Mushroom += 1) {
+				System.out.print("mushroom, ");
+			}
+			System.out.println();
+		}
+		for (Snake = 0; Snake < snake; Snake += 1) {
+			System.out.println(" a snake!");
+		}
+
+		// FOR LOOP. Start a for loop to repeat while age <= your age, like this:
+		// for ( age=0; age <= yourAge; age +=1 ) {
+
 	}
 
 }
---
+++ src/_05_for_loops/_4_banana_split/banana_split/banana_split.pde
@@ -0,0 +1,14 @@
+
+    
+size(500,500); 
+int ice = 200;
+for (int i =0; i < 3; i++){
+  text("ice cream", 230,ice); 
+ ice += 10; 
+
+
+}
+text("banana",230,230); 
+//for (int i =0; i < 1000; i++){
+    //println("bananas!"); 
+             
